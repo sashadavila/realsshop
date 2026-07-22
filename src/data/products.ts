766013cbdd4b5d6346902bd89813f74759195b74
Sasha Davila <sasha@example.com>
2026-07-22 14:29:05 -0300

    Corrijo imagenes del catalogo

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,108 +1,122 @@
 import type { Product } from "../types/Product";
 
+import bucanerasRojas from "../assets/images/bucanerasrojas.jpg";
+import cliterific from "../assets/images/cliterefic.jpg";
+import dildoDoble from "../assets/images/dildodoble.jpg";
+import fundaPeneana from "../assets/images/fundapeneana.jpg";
+import lubricanteJade from "../assets/images/lubjade.jpg";
+import mediasRedNegras from "../assets/images/mediasrednegras.jpg";
+import mediasRedRomboGrande from "../assets/images/mediasrednegrasrombogrande.jpg";
+import mediasStrassPiel from "../assets/images/mediasstraspiel.jpg";
+import pantyPortaligasStrass from "../assets/images/pantyportaligastras.jpg";
+import plugSiliconaVioleta from "../assets/images/plugsiliconavioleta.jpg";
+import portaligasRojo from "../assets/images/portaligasrojo.jpg";
+import sextual20mlRosas from "../assets/images/sextual20mlrosas.jfif";
+import sextual80mlRosas from "../assets/images/sextual80mlrosas.webp";
+
 export const products: Product[] = [
     {
         id: 1,
-        name: "bucaneras clasicas",
-        category: "lenceria",
-        image: "/src/assets/images/bucanerasrojas.jpg",
+        name: "Bucaneras clásicas",
+        category: "Lencería",
+        image: bucanerasRojas,
         price: 7000,
-        description: "color:rojo",
+        description: "Color rojo.",
     },
     {
         id: 2,
-        name: "cliterific",
-        category: "dildos",
-        image: "/src/assets/images/cliterefic.jpg",
+        name: "Cliterific",
+        category: "Dildos",
+        image: cliterific,
         price: 14000,
-        description: "color:piel.",
+        description: "Color piel.",
     },
     {
         id: 3,
-        name: "dildo doble",
-        category: "dildos",
-        image: "/src/assets/images/dildodoble.jpg",
+        name: "Dildo doble",
+        category: "Dildos",
+        image: dildoDoble,
         price: 16000,
-        description: "color:piel.",
+        description: "Color piel.",
     },
-     {
+    {
         id: 4,
-        name: "funda peneana",
-        category: "hombres",
-        image: "/src/assets/images/fundapeneana.jpg",
+        name: "Funda peneana",
+        category: "Hombres",
+        image: fundaPeneana,
         price: 12000,
-        description: "color:transparente.",
+        description: "Color transparente.",
     },
-       {
+    {
         id: 5,
-        name: "lubricante jade",
-        category: "lubricantes",
-        image: "/src/assets/images/lubjade.jpg",
+        name: "Lubricante Jade",
+        category: "Lubricantes",
+        image: lubricanteJade,
         price: 8500,
-        description: "compatible con silicona",
+        description: "Compatible con juguetes de silicona.",
     },
-      {
+    {
         id: 6,
-        name: "medias red",
-        category: "lenceria",
-        image: "/src/assets/images/mediasrednegras.jpg",
+        name: "Medias de red",
+        category: "Lencería",
+        image: mediasRedNegras,
         price: 8500,
-        description: "red medianas",
+        description: "Red mediana.",
     },
-         {
+    {
         id: 7,
-        name: "medias red",
-        category: "lenceria",
-        image: "/src/assets/images/mediasrednegrasrombogrande.jpg",
+        name: "Medias de red",
+        category: "Lencería",
+        image: mediasRedRomboGrande,
         price: 8500,
-        description: "red rombo grande",
+        description: "Red con rombo grande.",
     },
-             {
+    {
         id: 8,
-        name: "medias strass piel",
-        category: "lenceria",
-        image: "/src/assets/images/mediasstraspiel.jpg",
+        name: "Medias con strass color piel",
+        category: "Lencería",
+        image: mediasStrassPiel,
         price: 8500,
-        description: "red rombo chico",
+        description: "Red con rombo chico.",
     },
-                 {
+    {
         id: 9,
-        name: "pantys portaligas con strass",
-        category: "lenceria",
-        image: "/src/assets/images/pantyportaligastras.jpg",
+        name: "Panty portaligas con strass",
+        category: "Lencería",
+        image: pantyPortaligasStrass,
         price: 8500,
-        description: "red rombo chico",
+        description: "Red con rombo chico.",
     },
-                     {
+    {
         id: 10,
-        name: "plug de silicona con joya",
-        category: "juguetes",
-        image: "/src/assets/images/plugsiliconavioleta.jpg",
+        name: "Plug de silicona con joya",
+        category: "Juguetes",
+        image: plugSiliconaVioleta,
         price: 10000,
-        description: "color.violeta",
+        description: "Color violeta.",
     },
-                         {
+    {
         id: 11,
-        name: "portaligas rojo",
-        category: "lenceria",
-        image: "/src/assets/images/portaligasrojo.WEBP",
+        name: "Portaligas rojo",
+        category: "Lencería",
+        image: portaligasRojo,
         price: 3500,
-        description: "color.rojo",
+        description: "Color rojo.",
     },
-           {
+    {
         id: 12,
-        name: "lubricante anal aroma rosas ",
-        category: "lubricantes",
-        image: "/src/assets/images/sextual20mlrosas.jfif",
+        name: "Lubricante anal aroma a rosas",
+        category: "Lubricantes",
+        image: sextual20mlRosas,
         price: 4000,
-        description: "20ml compatible con silicona",
+        description: "Presentación de 20 ml. Compatible con juguetes de silicona.",
     },
-           {
-        id: 5,
-        name: "lubricante anal aroma a rosas",
-        category: "lubricantes",
-        image: "/src/assets/images/sextual80mlrosas.webp",
+    {
+        id: 13,
+        name: "Lubricante anal aroma a rosas",
+        category: "Lubricantes",
+        image: sextual80mlRosas,
         price: 8500,
-        description: "80ml compatible con silicona",
+        description: "Presentación de 80 ml. Compatible con juguetes de silicona.",
     },
 ];
